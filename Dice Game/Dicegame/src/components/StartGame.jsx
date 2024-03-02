59dc020b96475a553260a74bfa6f291ef64f0b62
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-03-02 11:58:35 +0500

    First Page Create

--- Dice Game/Dicegame/src/components/StartGame.jsx
+++ Dice Game/Dicegame/src/components/StartGame.jsx
@@ -1,5 +1,50 @@
+import styled from "styled-components";
+
 const StartGame = () => {
-  return <div>StartGame</div>;
+  return (
+    <Container>
+      <div>
+        <img src="/images/dices.png" />
+      </div>
+      <div className="content">
+        <h1>Dice Game</h1>
+        <Button>Play Now</Button>
+      </div>
+    </Container>
+  );
 };
 
 export default StartGame;
+
+const Container = styled.div`
+  max-width: 1180px;
+  height: 100vh;
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+
+  .content {
+    font-size: 48px;
+    white-space: nowrap;
+  }
+`;
+
+const Button = styled.button`
+  color: white;
+  padding: 10px 18px;
+  background: #000000;
+  border-radius: 5px;
+  min-width: 220px;
+  border: none;
+  font-size: 16px;
+  border: 1px solid transparent;
+  cursor: pointer;
+  transition: 0.3s background ease-in;
+
+  &:hover {
+    background-color: white;
+    border: 1px solid black;
+    color: black;
+    transition: 0.3s background ease-in;
+  }
+`;
