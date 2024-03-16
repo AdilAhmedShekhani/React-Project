da0b9c5fbe67cc423697aaf3774ac76b4ad7b9f7
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-03-16 11:08:36 +0500

    Navbar UI

--- Contact App/contactapp/src/App.jsx
+++ Contact App/contactapp/src/App.jsx
@@ -1,3 +1,20 @@
+import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
 export default function App() {
-  return <h1 className="flex text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="mx-auto max-w-[370px] px-4">
+      <Navbar />
+      <div className="flex gap-2">
+      <div className=" relative flex items-center flex-grow">
+        <FiSearch className=" ml-1 absolute text-3xl text-white" />
+        <input
+          type="text"
+          className="pl-9 h-10 flex-grow rounded-md border border-white bg-transparent text-white"
+        />
+      </div>
+        <AiFillPlusCircle className=" cursor-pointer text-5xl text-white" />
+      </div>
+    </div>
+  );
 }
---
+++ Contact App/contactapp/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="my-4 h-[60px] gap-2 rounded-lg bg-white flex items-center justify-center text-xl font-medium">
+      <img src="/firebase.svg" />
+      <h1>Firebase Contact App</h1>
+    </div>
+  );
+};
+
+export default Navbar;
