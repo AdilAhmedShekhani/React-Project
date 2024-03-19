0ae20010a812997ea2337305adb4309ece45ab9d
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-03-19 12:13:22 +0500

    Card UI Updated

--- Contact App/contactapp/src/App.jsx
+++ Contact App/contactapp/src/App.jsx
@@ -4,6 +4,9 @@ import { AiFillPlusCircle } from "react-icons/ai";
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./config/firebase";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { IoMdTrash } from "react-icons/io";
+import { RiEditCircleLine } from "react-icons/ri";
 export default function App() {
   const [contacts, setContacts] = useState([]);
 
@@ -39,9 +42,24 @@ export default function App() {
         </div>
         <AiFillPlusCircle className=" cursor-pointer text-5xl text-white" />
       </div>
-      <div>
+      <div className="mt-4">
         {contacts.map((contact) => (
-          <div key={contact.id}></div>
+          <div
+            key={contact.id}
+            className="bg-yellow flex items-center justify-around rounded-lg p-2"
+          >
+            <div className="flex gap-2">
+              <HiOutlineUserCircle className="text-orange text-4xl" />
+              <div className="">
+                <h2 className="font-medium">{contact.name}</h2>
+                <p className="text-sm">{contact.email}</p>
+              </div>
+            </div>
+            <div className="flex text-3xl">
+              <RiEditCircleLine />
+              <IoMdTrash className="text-orange" />
+            </div>
+          </div>
         ))}
       </div>
     </div>
