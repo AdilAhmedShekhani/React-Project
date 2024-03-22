ac641ca715b0d8ce94d36dd61ec6f652540b203d
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-03-22 11:06:55 +0500

    Validation Added

--- Contact App/contactapp/src/components/AddAndUpdateContact.jsx
+++ Contact App/contactapp/src/components/AddAndUpdateContact.jsx
@@ -1,8 +1,14 @@
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import Modal from "./Modal";
 import { addDoc, collection, doc, updateDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { toast } from "react-toastify";
+import * as Yup from "yup";
+
+const contactSchemaValidation = Yup.object().shape({
+  name: Yup.string().required("Name is Required"),
+  email: Yup.string().email("Invalid Email").required("Email is Required"),
+});
 
 const AddAndUpdateContact = ({ isOpen, onClose, isUpdate, contact }) => {
   const addContact = async (contact) => {
@@ -30,6 +36,7 @@ const AddAndUpdateContact = ({ isOpen, onClose, isUpdate, contact }) => {
     <div>
       <Modal isOpen={isOpen} onClose={onClose}>
         <Formik
+          validationSchema={contactSchemaValidation}
           initialValues={
             isUpdate
               ? {
@@ -50,10 +57,16 @@ const AddAndUpdateContact = ({ isOpen, onClose, isUpdate, contact }) => {
             <div className="flex flex-col gap-1">
               <label htmlFor="name">Name</label>
               <Field name="name" className="h-10 border" />
+              <div className="text-xs text-red-500">
+                <ErrorMessage name="name" />
+              </div>
             </div>
             <div className="flex flex-col gap-1">
               <label htmlFor="email">Email</label>
               <Field name="email" className="h-10 border" />
+              <div className="text-xs text-red-500">
+                <ErrorMessage name="email" />
+              </div>
             </div>
             <button className=" border bg-orange px-3 py-1.5 self-end">
               {isUpdate ? "update" : "add"} contact
