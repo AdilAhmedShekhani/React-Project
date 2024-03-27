02bcb193cfa236f84d6cc9190b82987914e9ab98
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-03-27 10:16:59 +0500

    Not Found Page Update

---
+++ Contact App/contactapp/src/components/NotFoundContact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundContact = () => {
+  return (
+    <div>
+      <img src="/contact.png" />
+      <h3>Contact Not Found</h3>
+    </div>
+  );
+};
+
+export default NotFoundContact;
